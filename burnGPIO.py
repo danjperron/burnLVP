aaeaa26f1a9893b72d398f376474697955e9c552
manojmo <manojmokashi@gmail.com>
2015-01-01 14:49:48 +0530

    Support for Multiple hardware interfaces
    Interfaces other than Raspberry Pi are supported. e.g. usb-serial. This file will now simply inlcude the interface-specific implementation file.

--- burnGPIO.py
+++ burnGPIO.py
@@ -3,38 +3,9 @@
 #  burnGPIO.py
 #
 #  GPIO definition interface
-#
-#  Put All your pin definition here
-#  All class will use this file for reference
-#
-#
-import RPi.GPIO as GPIO
+#  Provides methods to manipulate the programmer-hardware
+#  i.e the Clock, Data, MCLR and PGM pins
+#  Multiple implementations can exist.
+#  Import the verion for the interface you want
 
-global PIC_CLK
-global PIC_DATA
-global PIC_MCLR
-global PIC_PGM
-#CLK GPIO
-PIC_CLK = 7
-
-#DATA GPIO 
-PIC_DATA = 24
-
-#MCLR GPIO
-PIC_MCLR = 21
-
-#PGM
-PIC_PGM = 26
-
-
-def Setup_Interface():
-  GPIO.setwarnings(False)
-  GPIO.setmode(GPIO.BOARD)
-  GPIO.setup(PIC_MCLR,GPIO.OUT)
-  GPIO.output(PIC_MCLR,False)
-  GPIO.setup(PIC_CLK,GPIO.OUT)
-  GPIO.setup(PIC_DATA,GPIO.OUT)
-  GPIO.setup(PIC_PGM,GPIO.OUT)
-  
-
-
+from burnGPIO_USB2Ser import *
